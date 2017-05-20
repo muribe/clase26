fede184337a08ae5d08233b536a3ce77c522913e
Myckel <m.uribe@portal53.cl>
2017-05-19 21:55:08 -0400

    commit

--- alumno.rb
+++ alumno.rb
@@ -1,32 +1,20 @@
 class Alumno
-    # attr_accessor :alumno
-    def initialize()
-        @alumno = Array.new()
-        @avg_alumnos = Array.new()
-        4.times do |i|
-            @alumno[i] = Array.new(5) { |i2| (i2 > 0 ) ? (0..10).to_a.sample : "alumno#{i+1}" }
-        end
-    end
-    def avg_curso
-        avg_curso = 0
-        @alumno.each_with_index do |array,i|
-            sum = 0
-            array.each_with_index do |v,i| 
-                sum += v if v.to_i > 0   
-            end
-            @avg_alumnos.push(sum.to_f/4)
-            # puts "#{@avg_alumnos[i]}\n"
-            avg_curso += @avg_alumnos[i]
-        end
-        avg_curso/4
-    end
-    def max_avg
-        @alumno[@avg_alumnos.index(@avg_alumnos.max)][0]
-    end
+    attr_accessor :avg, :nombre
+    def initialize(nombre)
+        @nombre = nombre        
+        @notas = Array.new(5) { |i| (0..10).to_a.sample }
+        @avg = @notas.inject{ |sum, el| sum + el }.to_f / @notas.size
+     end
 end
 
-alumno = Alumno.new
-puts "El promedio del curso es: #{alumno.avg_curso.round(2)}\n"
-puts "El alumno con el promedio mas alto es: #{alumno.max_avg}"
+pedro = Alumno.new("Pedro")  
+juan = Alumno.new("Juan")  
+pepito = Alumno.new("Pepito")  
+jose = Alumno.new("Jose") 
 
+avg_curso = ((pedro.avg + juan.avg + pepito.avg + jose.avg) / 4).round(2)
+
+puts "El promedio del curso es: #{avg_curso}"
+nombre_mayor = ObjectSpace.each_object.select{|alumno| alumno.class == Alumno}.max_by{|alumno| alumno.avg}.nombre
+puts "El alumno con el mejor promedio es: #{nombre_mayor}"
 
